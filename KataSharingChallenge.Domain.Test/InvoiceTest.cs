453d2d43b001ea0559f5f50ba591dce1b321fded
tr00d <guillaume.faas@live.fr>
2023-07-26 11:45:28 +0200

    Challenge recorded :)

--- KataSharingChallenge.Domain.Test/InvoiceTest.cs
+++ KataSharingChallenge.Domain.Test/InvoiceTest.cs
@@ -1,5 +1,26 @@
-﻿namespace KataSharingChallenge.Domain.Test;
+﻿using FluentAssertions;
+
+namespace KataSharingChallenge.Domain.Test;
 
 public class InvoiceTest
 {
+    [Theory]
+    [InlineData(3, 1.21, "3.63 €")]
+    [InlineData(1, 1.21, "1.21 €")]
+    public void MultipleArticlesWithoutTax(int count, decimal unitPrice, string expected) =>
+        Invoice.Create(count, unitPrice).ToString().Should().Be(expected);
+
+    [Theory]
+    [InlineData(3, 1.21, 5, "3.81 €")]
+    [InlineData(3, 1.21, 20, "4.36 €")]
+    public void MultipleArticlesWithTax(int count, decimal unitPrice, decimal tax, string expected) =>
+        Invoice.Create(count, unitPrice).WithTax(tax).ToString().Should().Be(expected);
+
+    [Theory]
+    [InlineData(5, 345, 10, "1840.58 €")]
+    [InlineData(5, 200, 10, "1067.00 €")]
+    [InlineData(5, 1299, 10, "6787.28 €")]
+    [InlineData(5, 1000, 10, "5225.00 €")]
+    public void MultipleArticlesWithTaxAndDiscount(int count, decimal unitPrice, decimal tax, string expected) =>
+        Invoice.Create(count, unitPrice).WithTax(tax).ToString().Should().Be(expected);
 }
--- KataSharingChallenge.Domain/Invoice.cs
+++ KataSharingChallenge.Domain/Invoice.cs
@@ -1,5 +1,45 @@
-﻿namespace KataSharingChallenge.Domain;
+﻿using System.Globalization;
+using LanguageExt;
 
-public struct Invoice
+namespace KataSharingChallenge.Domain;
+
+public class Invoice
 {
+    private const decimal FivePercentDiscount = 0.95m;
+    private const decimal ThreePercentDiscount = 0.97m;
+    private const int DisplayedDecimals = 2;
+    private const int FivePercentDiscountThreshold = 5000;
+    private const int ThreePercentDiscountThreshold = 1000;
+    private readonly decimal grossAmount;
+    private readonly Option<decimal> tax;
+
+    private Invoice(decimal grossAmount) => this.grossAmount = grossAmount;
+
+    private Invoice(decimal grossAmount, decimal tax)
+        : this(grossAmount) => this.tax = tax;
+
+    public static Invoice Create(int count, decimal unitPrice) => new(count * unitPrice);
+
+    public override string ToString()
+    {
+        var totalAmount = this.grossAmount * this.EvaluateDiscountRate() * this.EvaluateTaxRate();
+        return FormatAmount(RoundAmount(totalAmount));
+    }
+
+    public Invoice WithTax(decimal tax) => new(this.grossAmount, tax);
+
+    private decimal EvaluateDiscountRate() =>
+        this.grossAmount switch
+        {
+            >= FivePercentDiscountThreshold => FivePercentDiscount,
+            >= ThreePercentDiscountThreshold => ThreePercentDiscount,
+            _ => 1,
+        };
+
+    private decimal EvaluateTaxRate() => this.tax.Match(some => 1 + some / 100, () => 1);
+
+    private static string FormatAmount(decimal roundedAmount) =>
+        $"{roundedAmount.ToString(CultureInfo.InvariantCulture)} €";
+
+    private static decimal RoundAmount(decimal totalAmount) => Math.Round(totalAmount, DisplayedDecimals);
 }
